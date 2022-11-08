123400ffc137f027c5927d1b3b9168b195e117c1
2yunseong <dbsdltjd3701@naver.com>
2022-11-08 19:32:15 +0900

    feat(my page): 기본 레이아웃 작성

--- src/MyPage/MyPage.jsx
+++ src/MyPage/MyPage.jsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { myPageData } from '../util/dummy';
 
-const MyPage = () => {};
+const MyPage = () => {
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    setUserData(() => myPageData);
+  }, []);
+
+  return (
+    <div className='place-self-center w-30 text-center'>
+      <div className='header flex'>
+        <h2>마이 페이지</h2>
+        <button className='b-4'>변경</button>
+      </div>
+      <div className='name-element flex'>
+        <div className='w-1/3'>이름</div>
+        <div>{userData.name}</div>
+      </div>
+      <div className='phone-element flex'>
+        <div className='w-1/3'>연락처</div>
+        <div>{userData.phone}</div>
+      </div>
+      <div className='email-element flex'>
+        <div className='w-1/3'>이메일</div>
+        <div>{userData.email}</div>
+      </div>
+    </div>
+  );
+};
 
 export default MyPage;
