0c5eaf3f70dd79c4ea76d8105449ba01b9a0902c
2yunseong <dbsdltjd3701@naver.com>
2022-11-08 17:32:13 +0900

    feat: mypage 라우팅

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 import Homepage from './Homepage/Homepage';
+import MyPage from './MyPage/MyPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/homepage" element={<Homepage />} />
+        <Route path='/' element={<Homepage />} />
+        <Route path='/homepage' element={<Homepage />} />
+        <Route path='/mypage' element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
