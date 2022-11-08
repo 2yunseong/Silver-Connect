67e3b7d10ade843491ef07986db16e876b2de9ac
loopy-lim <loopy@jnu.ac.kr>
2022-11-08 12:43:56 +0900

    feat: add calander layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
+import Homepage from './Homepage/Homepage';
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/homepage" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -1,11 +1,36 @@
-import Calendar from "@toast-ui/react-calendar";
-import Navbar from "../Navbar/Navbar";
-import "@toast-ui/calendar/dist/toastui-calendar.min.css";
+import Calendar from '@toast-ui/react-calendar';
+import './Homepage.css';
+import '@toast-ui/calendar/dist/toastui-calendar.min.css';
 
 function Homepage() {
   return (
-    <div>
-      <Navbar />
+    <div className="mx-8">
+      <div className="flex justify-between items-center">
+        <DatePicker />
+        <img className="h-full" src="./images/alertBar.png" alt="alertBar" />
+      </div>
+      <div id="calender">
+        <Calendar usageStatistics={false} view={'month'} />
+      </div>
+    </div>
+  );
+}
+
+function DatePicker() {
+  return (
+    <div className="flex mt-10 mb-10 px-10 py-2 bg-gray-100 w-fit rounded-full text-2xl font-bold">
+      <PickerTemplete defaultDate={'2022'} dateUnit={'년'} className="mr-6" />
+      <PickerTemplete defaultDate={'10'} dateUnit={'월'} />
+      <PickerTemplete defaultDate={'25'} dateUnit={'일'} />
+    </div>
+  );
+}
+
+function PickerTemplete(prop) {
+  return (
+    <div className="flex">
+      <div>{prop.defaultDate}</div>
+      <div>{prop.dateUnit}</div>
     </div>
   );
 }
