2635f1a2db7cb53286183b11c1943ee0857f03d0
2yunseong <dbsdltjd3701@naver.com>
2022-11-08 20:05:59 +0900

    feat(mypage): 버튼에 따른 변경 로직

--- src/MyPage/MyPage.jsx
+++ src/MyPage/MyPage.jsx
@@ -1,31 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { myPageData } from '../util/dummy';
+import LookUp from './Lookup';
+import Edit from './Edit';
 
 const MyPage = () => {
   const [userData, setUserData] = useState({});
+  const [isEdit, setIsEdit] = useState(false);
 
+  const onToggle = () => {
+    setIsEdit(() => !isEdit);
+  };
   useEffect(() => {
     setUserData(() => myPageData);
   }, []);
 
   return (
-    <div className='place-self-center w-30 text-center'>
-      <div className='header flex'>
+    <div className='my-page-container'>
+      <header className='flex'>
         <h2>마이 페이지</h2>
-        <button className='b-4'>변경</button>
-      </div>
-      <div className='name-element flex'>
-        <div className='w-1/3'>이름</div>
-        <div>{userData.name}</div>
-      </div>
-      <div className='phone-element flex'>
-        <div className='w-1/3'>연락처</div>
-        <div>{userData.phone}</div>
-      </div>
-      <div className='email-element flex'>
-        <div className='w-1/3'>이메일</div>
-        <div>{userData.email}</div>
-      </div>
+        <button onClick={onToggle}>{isEdit ? '완료' : '변경'}</button>
+      </header>
+      {isEdit ? (
+        <Edit />
+      ) : (
+        <LookUp
+          name={userData.name}
+          phone={userData.phone}
+          email={userData.email}
+        />
+      )}
     </div>
   );
 };
