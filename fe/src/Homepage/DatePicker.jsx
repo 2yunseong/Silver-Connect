1101d99b690297093086747bce093eb4584659d2
loopy-lim <loopy@jnu.ac.kr>
2022-11-24 14:02:32 +0900

    feat(calender): 안에 calender에 데이터를 뿌린다

--- fe/src/Homepage/DatePicker.jsx
+++ fe/src/Homepage/DatePicker.jsx
@@ -1,11 +1,11 @@
-function DatePicker() {
+function DatePicker(prop) {
   return (
     <div className="flex mt-10 mb-10 px-10 py-2 bg-gray-100 w-fit rounded-full text-2xl font-bold">
       <PickerTemplete defaultDate={'2022'} dateUnit={'년'} />
-      <PickerTemplete defaultDate={'10'} dateUnit={'월'} />
-      <PickerTemplete defaultDate={'25'} dateUnit={'일'} />
-    </div>
-  );
+      <PickerTemplete defaultDate={'12'} dateUnit={'월'} />
+      <PickerTemplete defaultDate={'1'} dateUnit={'일'} />
+      </div>
+      );
 }
 
 function PickerTemplete(prop) {
--- fe/src/Homepage/SCCalender.jsx
+++ fe/src/Homepage/SCCalender.jsx
@@ -1,24 +1,30 @@
 import Calendar from '@toast-ui/react-calendar';
 import DatePicker from './DatePicker';
+import useSWR from 'swr';
 import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+import fetcher from '../util/fetcher';
+import { createRef } from 'react';
 
 function SCCalender() {
-  const initialEvents = [
-    {
-      id: '1',
+  const { data, error } = useSWR(
+    'http://133.186.219.125:8080/api/calendar/1/month/12',
+    fetcher,
+  );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>
+
+  const initialEvents = data.map((resident) => {
+    return {
+      id: resident.id,
       calendarId: 'cal1',
-      title: '박봉선',
-      start: new Date().toDateString(),
-      end: new Date().toDateString(),
-    },
-    {
-      id: '2',
-      calendarId: 'cal1',
-      title: '최득춘',
-      start: new Date().toDateString(),
-      end: new Date().toDateString(),
-    },
-  ];
+      title: resident.residentName,
+      start: new Date(+resident.epochTime * 1000),
+      end: new Date(+(resident.epochTime + 3600) * 1000),
+    };
+  });
+
+  const calendarRef = createRef();
+  console.log(calendarRef.current);
 
   return (
     <div className="mx-8 w-3/5">
@@ -31,6 +37,10 @@ function SCCalender() {
           usageStatistics={false}
           view={'month'}
           events={initialEvents}
+          disableDblClick={false}
+          disableClick={false}
+          isReadOnly={true}
+          ref={calendarRef}
         />
       </div>
     </div>
