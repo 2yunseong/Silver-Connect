fc7baf369fe119e41fcf041f22c834d5ef196aa4
2yunseong <dbsdltjd3701@naver.com>
2022-11-28 16:43:33 +0900

    feat(SignUp): 회원가입 페이지 CSS 변경

--- fe/src/SignUp/SignUp.jsx
+++ fe/src/SignUp/SignUp.jsx
@@ -29,59 +29,59 @@ const SignUp = () => {
   };
 
   return (
-    <div className='sign-up-container mx-auto my-10 justify-center text-center rounded-lg border'>
-      <h1 className='font-bold text-5xl m-20'>회원가입</h1>
-      <div className='flex flex-col w-100'>
-        <div className='flex justify-between text-justify w-2/3 mx-auto text-3xl my-6'>
+    <div className='sign-up-container mx-auto my-10 justify-center text-center rounded-lg border shadow-lg'>
+      <h1 className='font-bold text-5xl m-20'>회원 가입</h1>
+      <div className='flex flex-col w-full'>
+        <div className='flex justify-center text-justify w-3/5 mx-auto text-2xl my-6'>
           <span className='w-1/3'>이름:</span>
           <input
-            className='w-2/3 border'
+            className='border-b pb-2'
             type='text'
             placeholder='이름'
             onChange={name.onChange}
             value={name.value}
           />
         </div>
-        <div className='flex justify-between text-justify w-2/3 mx-auto text-3xl my-6'>
+        <div className='flex justify-center text-justify w-3/5 mx-auto text-2xl my-6'>
           <span className='w-1/3'>연락처:</span>
           <input
-            className='w-2/3 border'
+            className='border-b pb-2'
             type='text'
             placeholder='ex) 010-1234-1234'
             onChange={phoneNumber.onChange}
             value={phoneNumber.value}
           />
         </div>
-        <div className='flex justify-between text-justify w-2/3 mx-auto text-3xl my-6'>
+        <div className='flex justify-center text-justify w-3/5 mx-auto text-2xl my-6'>
           <span className='w-1/3'>이메일(ID):</span>
           <input
-            className='w-2/3 border'
+            className='border-b pb-2'
             type='text'
             placeholder='example@jnu.ac.kr'
             onChange={email.onChange}
             value={email.value}
           />
         </div>
-        <div className='flex justify-between text-justify w-2/3 mx-auto text-3xl my-6'>
+        <div className='flex justify-center text-justify w-3/5 mx-auto text-2xl my-6'>
           <span className='w-1/3'>비밀번호:</span>
           <input
-            className='w-2/3 border'
+            className='border-b pb-2'
             type='password'
             placeholder='비밀번호'
             onChange={password.onChange}
             value={password.value}
           />
         </div>
-        <div className='flex justify-between text-justify w-2/3 mx-auto text-3xl my-6'>
+        <div className='flex justify-center text-justify w-3/5 mx-auto text-2xl my-6'>
           <span className='w-1/3'>비밀번호 확인:</span>
           <input
-            className='w-2/3 border'
+            className='border-b pb-2'
             type='password'
-            placeholder='비밀번호'
+            placeholder='비밀번호 확인'
           />
         </div>
         <button
-          className='text-3xl text-white bg-blue-700 border-blue rounded-lg w-36 mx-auto my-10 h-12'
+          className='text-white bg-blue-500 border-blue rounded-xl w-96 mx-auto mt-8 mb-20 h-12'
           onClick={onSignUp}
         >
           회원가입
