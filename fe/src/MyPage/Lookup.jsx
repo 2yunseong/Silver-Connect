ea09c57278065290f615d87f5b38d4fa8a2581bc
2yunseong <dbsdltjd3701@naver.com>
2022-11-28 16:45:57 +0900

    feat(Lookup): 입력 창 css 수정

--- fe/src/MyPage/Lookup.jsx
+++ fe/src/MyPage/Lookup.jsx
@@ -4,21 +4,21 @@ import profile from './profile.png';
 
 const LookUp = ({ name, phone, email }) => {
   return (
-    <div className='text-2xl font-semibold'>
+    <div className='text-xl font-normal'>
       <img
-        className='rounded-full w-50 p-3 mx-auto mt-0 mb-10'
+        className='rounded-full w-60 mx-auto mt-10 mb-5'
         src={profile}
         alt='profile'
       />
-      <div className='name-element flex py-5'>
+      <div className='name-element flex pt-10 pb-2 border-b'>
         <div className='w-1/3'>이름</div>
         <div className='w-2/3'>{name}</div>
       </div>
-      <div className='phone-element flex py-5'>
+      <div className='phone-element flex pt-10 pb-2 border-b'>
         <div className='w-1/3'>연락처</div>
         <div className='w-2/3'>{phone}</div>
       </div>
-      <div className='email-element flex py-5'>
+      <div className='email-element flex pt-10 pb-2 border-b'>
         <div className='w-1/3'>이메일</div>
         <div className='w-2/3'>{email}</div>
       </div>
