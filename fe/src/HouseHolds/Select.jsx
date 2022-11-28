7a6386f62ff485d6a304fae08c2d2826106f1b83
2yunseong <dbsdltjd3701@naver.com>
2022-11-28 15:52:26 +0900

    feat(Selet): 가구 선택 css 수정

--- fe/src/HouseHolds/Select.jsx
+++ fe/src/HouseHolds/Select.jsx
@@ -1,6 +1,8 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import useSWR from 'swr';
 import fetcher from '../util/fetcher';
+import residentImg from './residentImg.jpg';
+import './Select.css';
 
 const Select = () => {
   const params = useParams();
@@ -9,52 +11,70 @@ const Select = () => {
     `http://133.186.219.125:8080/api/guardian/${params.id}`,
     fetcher
   );
+  const types = {
+    SAFE: '안전',
+    WARN: '인지',
+    DANGER: '위험',
+    EMERGENCY: '긴급',
+  };
 
   if (error) return <div>error!</div>;
   if (!data) return <div>no data</div>;
 
   return (
-    <div className='flex flex-col w-3/5 h-2/5 relative top-24 left-80 border text-center pt-5 pb-20 px-20 rounded-lg font-semibold'>
-      <button
-        className='relative right-80'
-        onClick={() => {
-          navigate('/household');
-        }}
-      >{`<`}</button>
-      <h2 className='font-bold text-3xl mb-8'>정보</h2>
-      <div className='flex py-3'>
-        <div className='w-1/3'>이름</div>
-        <div className='w-2/3'>{data.household.residentName}</div>
-      </div>
-      <div className='flex py-3'>
-        <div className='w-1/3'>주소</div>
-        <div className='w-2/3'>{data.household.address}</div>
-      </div>
-      <div className='flex py-3'>
-        <div className='w-1/3'>연세</div>
-        <div className='w-2/3'>{data.household.residentAge}세</div>
-      </div>
-      <div className='flex py-3'>
-        <div className='w-1/3'>연락처</div>
-        <div className='w-2/3'>{data.household.residentPhoneNumber}</div>
-      </div>
-      <hr />
-      <div>
-        <div className='flex py-3'>
-          <div className='w-1/3'>보호자</div>
+    <>
+      <div className='select-container flex justify-center items-center flex-col border rounded-lg py-10 shadow-lg'>
+        <div className='font-bold text-5xl py-3'>어르신 가구 정보</div>
+
+        <img
+          className='rounded-full w-60 h-60 my-7 my-0 mx-auto'
+          src={residentImg}
+          alt='profile'
+        />
+        <div className='flex py-3 w-3/5 bmx-auto my-0 border-b'>
+          <div className='w-1/3 font-bold'>이름</div>
+          <div className='w-2/3'>{data.household.residentName}</div>
+        </div>
+        <div className='flex py-3 w-3/5 mx-auto my-0 border-b'>
+          <div className='w-1/3 font-bold'>주소</div>
+          <div className='w-2/3'>{data.household.address}</div>
+        </div>
+        <div className='flex py-3 w-3/5 mx-auto my-0 border-b'>
+          <div className='w-1/3 font-bold'>연세</div>
+          <div className='w-2/3'>{data.household.residentAge}세</div>
+        </div>
+        <div className='flex py-3 w-3/5 mx-auto my-0 border-b'>
+          <div className='w-1/3 font-bold'>연락처</div>
+          <div className='w-2/3'>{data.household.residentPhoneNumber}</div>
+        </div>
+        <div className='flex py-3 w-3/5 mx-auto my-0 border-b'>
+          <div className='w-1/3 font-bold'>보호자</div>
           <div className='w-2/3'>{data.name}</div>
         </div>
-        <div className='flex py-3'>
-          <div className='w-1/3'>보호자 연락처</div>
+        <div className='flex py-3 w-3/5 mx-auto my-0 border-b'>
+          <div className='w-1/3 font-bold'>보호자 연락처</div>
           <div className='w-2/3'>{data.phoneNumber}</div>
         </div>
         <hr />
-        <div className='flex py-3'>
-          <div className='w-1/3'>위험도</div>
-          <div className='w-2/3'>{data.household.risk}</div>
+        <div className='flex py-3 w-3/5 mx-auto my-0 border-b'>
+          <div className='w-1/3 font-bold'>위험도</div>
+          <div className='flex'>
+            <div
+              className={`w-6 h-89 rounded-full mr-2 ${data.household.risk}`}
+            />
+            <div>{types[data.household.risk]}</div>
+          </div>
         </div>
+        <button
+          className='text-2xl text-white bg-blue-500 border-blue rounded-lg w-36 mx-auto mt-10 py-3'
+          onClick={() => {
+            navigate('/household');
+          }}
+        >
+          뒤로가기
+        </button>
       </div>
-    </div>
+    </>
   );
 };
 
